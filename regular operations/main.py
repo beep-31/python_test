c70922ae78edb901e4eb3eed0f89369de76d5c89
beep-31 <ascheglov17@gmail.com>
2020-06-08 15:13:21 +0200

    Regular expression program is finished

--- regular operations/main.py
+++ regular operations/main.py
@@ -1,6 +1,6 @@
 import re, pyperclip
 
-phoneRegex = re.compile(r'''(
+phoneRegex = re.compile('''(
 	(\d{3}|\(\d{3}\))?
 	(\s|-|\.)?
 	(\d{3})
@@ -9,14 +9,30 @@ phoneRegex = re.compile(r'''(
 	(\s*(ext|x|ext.)\s*(\d{2,5}))?
 	)''', re.VERBOSE)
 
+	
+
 emailRegex = re.compile(r'''(
-	[a-z A-z 0-9 ._%+-]))+
+	[a-zA-z0-9._%+-]+
 	@
-	[a-z A-Z 0-9.-]+
+	[a-zA-Z0-9.-]+
 	(\.[a-zA-Z]{2,4})
 	)''', re.VERBOSE)
+
 
 text = str(pyperclip.paste())
 matches = []
 for groups in phoneRegex.findall(text):
-	print(groups)
+	phoneNum = "-".join([groups[1], groups[3], groups[5]])
+	if groups[8] != '':
+		phoneNum += 'x' + groups[8]
+	matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+	matches.append(groups[0])
+
+if len(matches)>0:
+	pyperclip.copy("\n".join(matches))
+	print("Copied to the buffer")
+	print('\n'.join(matches))
+else:
+	print("Nothing has been found")
+
