b4d63f275b4ca224952709e6f022dcfc901c7019
Alex <ascheglov17@gmail.com>
2020-06-11 17:31:31 +0200

    New commit

---
+++ working with files/ZipBackup.py
@@ -0,0 +1,32 @@
+import zipfile, os, shutil
+
+folderToSave = "C:\\Users\\admin\\Desktop"
+
+def backupToZip(folder):
+	cwd = os.getcwd()
+	folder = os.path.abspath(folder)
+	num = 1
+	while True:
+		zipFileName = os.path.basename(folder) + '_' + str(num) + '.zip'
+		if not os.path.exists(zipFileName):
+			break
+		num = num + 1
+
+	print("File to create: %s" % (zipFileName))
+	backupZip = zipfile.ZipFile(zipFileName, 'w')
+	#обход всего дерева папки
+	for foldername, subfolders, filenames in os.walk(folder):
+		print('Adding files from %s folder' % (foldername))
+		backupZip.write(foldername)
+		for filename in filenames:
+			newBase = os.path.basename(folder) + '_'
+			if filename.endswith('.zip') and filename.startswith(newBase):
+				continue
+			backupZip.write(os.path.join(foldername, filename))
+	backupZip.close()
+	print('Finished')
+	shutil.move(os.path.join(cwd, zipFileName), folderToSave)
+
+folder = input("Write down a full path to the folder you want to zip >> ")
+
+backupToZip(folder)
